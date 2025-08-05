f941fe622650a1c9ce8dea8d2c2f4edfc5a4f21d
Vivek <vivekbisht4320@gmail.com>
2025-08-05 09:28:22 +0530

    Saved file created via form to uploads

--- server/controllers/controllers.js
+++ server/controllers/controllers.js
@@ -1,11 +1,41 @@
+const { log } = require('console');
+const fs = require('fs');
+const path = require('path');
+
 const multerController = (req,res)=>{
     console.log("File recieved : " , req.file);
     return res.send("File received");
 }
 
-const formController = (req,res)=>{
-    console.log(req.body.fileName);
+const formController = async (req,res)=>{
+    if (!fs.existsSync(path.join(__dirname, "../public/uploads"))) {
+          await fs.mkdir(path.join(__dirname, "../public/uploads"), (err) => {
+            if (err) throw err;
+          });
+        }  
     
+    let fileName = req.body.fileName;
+
+    if(!fileName.endsWith('.md')){
+        fileName = fileName.split(" ").join("_").trim();
+        fileName = fileName + "-" + Math.round(Math.random() * 1e4);
+        fileName += ".md";
+    }
+    else{
+        fileName = fileName.split(" ").join("_").trim();
+        const fileextension = path.extname(fileName);
+        const uniqueSuffix = "-" + Math.round(Math.random() * 1e4);
+        fileName = fileName.split(".").slice(0, -1).join(".") + uniqueSuffix + fileextension;
+    }
+
+    const filePath = path.join(__dirname , `../public/uploads/${fileName}`);
+    const fileData = `${req.body.markdownText}`
+    
+
+    fs.appendFile(filePath,fileData,(err)=>{
+        if(err) throw err;
+        console.log("File created..");
+    })
 }
 
 module.exports = {multerController,formController};
